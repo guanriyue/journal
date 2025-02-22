f7768b26168079460afd989334a844b91c42f2b9
guanriyue <wangyuze0319@outlook.com>
2025-02-22 14:59:59 +0800

    feat(hooks): 调整 useComposedRef 类型定义

--- packages/react/hooks/src/useComposedRefs.ts
+++ packages/react/hooks/src/useComposedRefs.ts
@@ -1,7 +1,13 @@
 // import { composeRefs as _composeRefs, useComposedRefs } from '@radix-ui/react-compose-refs';
 
+import { useComposedRefs } from '@radix-ui/react-compose-refs';
+
 // export const composeRefs = _composeRefs as <T>(...refs: (React.Ref<T> | undefined)[]) => (instance: T | null) => void | React.DO_NOT_USE_OR_YOU_WILL_BE_FIRED_CALLBACK_REF_RETURN_VALUES[keyof React.DO_NOT_USE_OR_YOU_WILL_BE_FIRED_CALLBACK_REF_RETURN_VALUES]
 
 // export const useComposedRef = useComposedRefs as <T>(...refs: (React.Ref<T> | undefined)[]) => (instance: T | null) => void | React.DO_NOT_USE_OR_YOU_WILL_BE_FIRED_CALLBACK_REF_RETURN_VALUES[keyof React.DO_NOT_USE_OR_YOU_WILL_BE_FIRED_CALLBACK_REF_RETURN_VALUES]
 
-export { composeRefs, useComposedRefs as useComposedRef } from '@radix-ui/react-compose-refs';
+export { composeRefs  } from '@radix-ui/react-compose-refs';
+
+export const useComposedRef = useComposedRefs as (
+  <T>(...refs: (React.Ref<T> | undefined)[]) => (instance: T | null) => void
+);
